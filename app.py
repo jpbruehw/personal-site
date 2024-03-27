fe2958411b8cbeb1a8c7d21600d6b5a96ea2fcfa
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2024-03-27 18:32:17 +0100

    commit of v1 code

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template, request
+
+# initialize the app
+app = Flask(__name__)
+
+# define routes
+@app.route('/', methods=["GET"])
+def home():
+    """Render homepage """
+    
+    # set page title
+    title = 'Welcome!'
+
+    return render_template('home.html', title=title)
+
