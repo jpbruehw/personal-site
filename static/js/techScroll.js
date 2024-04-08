7ac4e2ee8e0ee8a3761e6bdbc84f9e51e7daed2e
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2024-04-08 12:09:24 +0200

    made new changes to css styling and added technical skills section

---
+++ static/js/techScroll.js
@@ -0,0 +1,29 @@
+/** functionality for the scrolling effect
+ *  of the technology section
+ */
+$(document).ready(() => {
+  // select scrollers
+  const scrollers = $(".scroller");
+  console.log("PRINT SCROLLER: ", scrollers);
+
+  // function to perform animation
+  const addAnimation = () => {
+    scrollers.each(function () {
+      $(this).attr("data-animated", true);
+      // get the images
+      const scrollerInner = $(".scroller__inner");
+      const scrollerContent = scrollerInner.children().toArray();
+      // returns js array so regular forEach
+      scrollerContent.forEach(function (element) {
+        const duplicatedItem = $(element).clone();
+        duplicatedItem.attr("aria-hidden", true);
+        scrollerInner.append(duplicatedItem);
+      });
+    });
+  };
+
+  // make check for media query if user has reduced motion on
+  if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+  }
+});
